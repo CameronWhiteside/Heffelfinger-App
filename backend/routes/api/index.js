18b22b8206342510d581681eb5c6013afd4a7a08
Cameron Whiteside <whiteside.cameron@gmail.com>
2022-01-01 17:33:36 -0700

    finished session api routes to log in, log out, and restore user

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,11 +3,13 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
-
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
+
+
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
 
 module.exports = router;
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,6 +31,29 @@ router.post(
     }),
 );
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+);
+
+
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+
 
 
 module.exports = router;
