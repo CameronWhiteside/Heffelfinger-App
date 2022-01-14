361d42cc508d336abc13aebb444ed54f7c50e984
Cameron Whiteside <whiteside.cameron@gmail.com>
2022-01-14 00:18:06 -0700

    finished landing page

--- frontend/src/components/Basic/Navigation/index.js
+++ frontend/src/components/Basic/Navigation/index.js
@@ -117,9 +117,9 @@ function Navigation({ isLoaded }){
     <div className='navigation'>
       <div className='text-links'>
         {/* <NavLink exact to="/">Home</NavLink> */}
-        <NavLink exact to="/users">Users</NavLink>
-        <NavLink exact to="/companies">Companies</NavLink>
-        <NavLink exact to="/events">Events</NavLink>
+        {/* <NavLink exact to="/users">Users</NavLink> */}
+        <NavLink exact to="/companies">Projects</NavLink>
+        {/* <NavLink exact to="/events">Events</NavLink> */}
         <NavLink exact to="/about">About</NavLink>
       </div>
       <div className='site-logo'>
--- frontend/src/components/RegisterLogin/Splash/WelcomePage.js
+++ frontend/src/components/RegisterLogin/Splash/WelcomePage.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react"
-import { Redirect, useLocation } from "react-router-dom";
+import { NavLink, Redirect, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import LoginFormPage from "../LoginFormPage";
 import SignInRegisterCombo from "./SignInRegisterCombo";
@@ -21,7 +21,38 @@ const WelcomePage = ({ newUserDefault }) => {
                 <div className="welcome-left">
                     <div className="welcome image-container">
                         {!sessionUser && <img onMouseDown={(e)=> e.preventDefault()} src="/assets/greeting-letter-925.png" alt='weclome letter'/>}
-                        {sessionUser && <img onMouseDown={(e)=> e.preventDefault()} src="/assets/handshake.png" alt='handshake'/>}
+                        {sessionUser &&
+                            <div className="welcome-links">
+                                {/* <h1 className="wide-welcome">{`Welcome`}</h1> */}
+                                <div className="badge-holder">
+                                    <NavLink to='/companies'>
+                                        <div className="badge-link company-badge"></div>
+                                    </NavLink>
+                                    <NavLink to='/about'>
+                                        <div className="badge-link about-badge"></div>
+                                    </NavLink>
+                                    <NavLink to='/events' disabled>
+                                        <div className="badge-link events-badge">
+                                            <div className="coming-soon-icon">
+                                                <div/>
+                                            </div>
+                                        </div>
+                                    </NavLink>
+                                    <NavLink to='/users' disabled>
+                                        <div className="badge-link users-badge">
+                                            <div className="coming-soon-icon">
+                                                <div/>
+                                        </div>
+                                        </div>
+                                    </NavLink>
+                                    <div>
+
+                                    </div>
+
+                                </div>
+                                {/* <h4 className="path-we">{`Select Your Path`}</h4> */}
+                            </div>
+                        }
                     </div>
                 </div>
                 <div className="welcome-right">
